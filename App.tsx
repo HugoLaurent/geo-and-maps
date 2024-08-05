6dc19259068434c64a911c46728cc5b185c81ff2
HugoLaurent <lauhugopro@gmail.com>
2024-08-05 15:31:59 +0200

    Step 'geolocalisation map'

--- App.tsx
+++ App.tsx
@@ -1,63 +1,12 @@
-import { useState, useEffect } from "react";
-import { Text, View } from "react-native";
-import * as Location from "expo-location";
-
+import React from "react";
+import { View, StatusBar } from "react-native";
+import MapView from "react-native-maps";
 import styles from "./styles";
 
-const GOOGLE_MAPS_API_KEY = "AIzaSyAXqRpJoapii4U3psCEIZoIezMbv8fGzSI";
-const URL = `https://maps.google.com/maps/api/geocode/json?key=${GOOGLE_MAPS_API_KEY}&latlng=`;
+StatusBar.setBarStyle("dark-content");
 
-export default function App() {
-  const [address, setAddress] = useState("Loading...");
-  const [longitude, setLongitude] = useState<number | undefined>();
-  const [latitude, setLatitude] = useState<number | undefined>();
-
-  useEffect(() => {
-    function setPosition({
-      coords: { latitude, longitude },
-    }: Location.LocationObject) {
-      setLatitude(latitude);
-      setLongitude(longitude);
-
-      fetch(`${URL}${latitude},${longitude}`)
-        .then((response) => response.json())
-        .then(({ results }) => {
-          if (results.length > 0) {
-            setAddress(results[0].formatted_address);
-          }
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    }
-
-    let watcher: Location.LocationSubscription;
-
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== "granted") {
-        return;
-      }
-
-      let location = await Location.getCurrentPositionAsync({});
-      setPosition(location);
-
-      watcher = await Location.watchPositionAsync(
-        { accuracy: Location.LocationAccuracy.Highest },
-        setPosition
-      );
-    })();
-
-    return () => {
-      watcher.remove();
-    };
-  }, []);
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.label}>Address: {address}</Text>
-      <Text style={styles.label}>Latitude: {latitude}</Text>
-      <Text style={styles.label}>Longitude: {longitude}</Text>
-    </View>
-  );
-}
+export default () => (
+  <View style={styles.container}>
+    <MapView style={styles.mapView} showsUserLocation followsUserLocation />
+  </View>
+);
--- styles.ts
+++ styles.ts
@@ -3,15 +3,14 @@ import { StyleSheet } from "react-native";
 export default StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
     justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "ghostwhite",
   },
-  label: {
-    textAlign: "center",
-    margin: 10,
-  },
-  address: {
-    fontWeight: "bold",
+
+  mapView: {
+    alignSelf: "stretch",
+    height: 400,
+    margin: 15,
   },
 });
