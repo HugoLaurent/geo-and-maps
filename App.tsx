f9dc96e1add5e8cc736b5bf9b831eb5887e7a0b3
HugoLaurent <lauhugopro@gmail.com>
2024-08-05 15:36:02 +0200

    Step 'geolocalisation avec cloud campus'

--- App.tsx
+++ App.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { View, StatusBar } from "react-native";
-import MapView from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
 import styles from "./styles";
 
 StatusBar.setBarStyle("dark-content");
 
 export default () => (
   <View style={styles.container}>
-    <MapView style={styles.mapView} showsUserLocation followsUserLocation />
+    <MapView style={styles.mapView} showsUserLocation followsUserLocation>
+      <Marker
+        title="Cloud Campus"
+        description="Cloud Campus"
+        coordinate={{ latitude: 48.8585802, longitude: 2.3730884 }}
+      />
+    </MapView>
   </View>
 );
